8ae89ec11b7da3ed3206c345dfd7c76443f2cfbb
mridul2303 <mridul2303@gmail.com>
2025-08-24 22:31:07 +0530

    did leet code que

--- NumberSystem/src/Med1.java
+++ NumberSystem/src/Med1.java
@@ -1,32 +1,69 @@
 import java.util.Scanner;
 
-//count Primes
 public class Med1 {
     public static void main(String[] args) {
-
-    Scanner inp = new Scanner(System.in);
-    int n = inp.nextInt();
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
         System.out.println(countPrimes(n));
-}
-static int countPrimes(int n ){
-int count = 1;
-while (n>1){
-   if (isPrime(n)){
-       count++;
-       n--;
-   }
-   else {
-       n--;
+    }
 
-   }}
-return count;
-}
-static Boolean isPrime(int n ){
-        if (n%2==0){
-            return false;
-        }
-        for (int i = 3 ; i*i < n ; i++  ){
-            if (n%i==0){
-
+    static int countPrimes(int n) {
+        int count = 0;
+        while (n > 1) {
+            if (isPrime(n)) {
+                count++;
             }
-        }}}
+            n--;
+        }
+        return count;
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//efficient version
+//import java.util.Scanner;
+//
+//public class Med1 {
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//        int n = inp.nextInt();
+//        System.out.println(countPrimes(n));
+//    }
+//
+//    static int countPrimes(int n) {
+//        int count = 0;
+//        while (n > 1) {
+//            if (isPrime(n)) {
+//                count++;
+//            }
+//            n--;
+//        }
+//        return count;
+//    }
+//
+//    static boolean isPrime(int n) {
+//        if (n <= 1) return false;
+//        if (n == 2) return true;
+//        if (n % 2 == 0) return false;
+//
+//        for (int i = 3; i * i <= n; i += 2) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
