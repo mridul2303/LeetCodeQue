7f7d65518e6eac8b670e81687cdf9905781135ed
mridul2303 <mridul2303@gmail.com>
2025-09-12 17:18:38 +0530

    done a leet code question

--- Array/src/Med4.java
+++ Array/src/Med4.java
@@ -1,1 +1,16 @@
-
+//reverse integer
+public class Med4 {
+    public static void main(String[] args) {
+        int x = -123;
+        System.out.println(reverse(x));
+    }
+    static int reverse(int x){
+        int m = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            m = m * 10 + digit;
+            x /= 10;
+        }
+        return m;
+    }
+}
